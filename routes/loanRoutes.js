1ca6e6f34e1b1e499dd3ee3a5b9924bebf737c67
jatin2000416kumar <jatin_2301ec@iitp.ac.in>
2025-03-16 13:48:16 +0530

    added loan routes and loan controller

---
+++ routes/loanRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const loanController = require('../controllers/loanController');
+const authenticate = require('../middleware/authMiddleware');
+const { isVerified, isAdmin } = require('../middleware/roleMiddleware');
+
+// All loan routes require authentication
+router.use(authenticate);
+
+// User routes
+router.post('/apply', isVerified, loanController.applyLoan);
+router.get('/my-loans', isVerified, loanController.getUserLoans);
+router.get('/:loanId', isVerified, loanController.getLoanDetails);
+
+// Admin routes
+router.put('/:loanId/process', isAdmin, loanController.processLoanApplication);
+
+module.exports = router;
